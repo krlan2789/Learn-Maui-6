6efad2776d62617cae190dc7003d80f890fc873c
Erlan Kurnia <33871129+krlan2789@users.noreply.github.com>
2024-08-09 15:28:13 +0700

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,7 +2,7 @@
 {
     public partial class App : Application
     {
-        public static PersonRepository PersonRepo { get; set; }
+        public static PersonRepository? PersonRepo { get; set; }
 
         public App(PersonRepository repo)
         {
